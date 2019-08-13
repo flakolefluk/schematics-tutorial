fb9db5271449bb42479895e0bb95d674770d3d6a
Ignacio Falk <flakolefluk@gmail.com>
2019-08-13 06:06:55 -0400

    adds external schematic

---
+++ src/external/index.ts
@@ -0,0 +1,25 @@
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  chain,
+  externalSchematic
+} from "@angular-devkit/schematics";
+
+export function external(options: any): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    return chain([
+      externalSchematic("@schematics/angular", "component", {
+        project: options.project
+      }),
+      extend()
+    ])(tree, context);
+  };
+}
+
+export function extend(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    context.logger.info("Extending schematic");
+    return tree;
+  };
+}
